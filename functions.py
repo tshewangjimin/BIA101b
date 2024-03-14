44c3917edd3eebf818202d149b149cc7db4a11b5
tshewangjimin <03230393.gcbs@rub.edu.bt>
2024-03-14 15:21:38 +0600

    functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+
+def greet():
+    # do some calculation code 
+    # complex stuffs
+    print('Hello')
+
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print('adding numbers')
+    return sum
+
+userNumber = 10
+userNumber2 = 20
+
+returnedSum = add_numbers(userNumber, userNumber2)
+print(returnedSum)
